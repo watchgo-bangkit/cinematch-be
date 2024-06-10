fe792e8bbef6670ed60acf1800e69c581353b3d5
IvanLeovandi <ivanleovandi@gmail.com>
2024-06-10 22:49:25 +0700

    fix: remove userId params from route and add user verification by checking userId to db

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,12 +1,14 @@
+import { PrismaClient } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import HttpError from '../utils/httpError';
+
+const prisma = new PrismaClient();
 
 export interface AuthRequest extends Request {
     user?: { userId: number; email: string };
 }
 
-const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+const authenticateToken = async (req: AuthRequest, res: Response, next: NextFunction) => {
     const token = req.header('Authorization')?.split(' ')[1];
 
     if (!token) {
@@ -16,6 +18,18 @@ const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) 
     try {
         const secret = process.env.JWT_SECRET || 'secretKey';
         const decoded = jwt.verify(token, secret) as { userId: number; email: string };
+
+        // Check if user exists in the database
+        const user = await prisma.user.findUnique({
+        where: {
+            id: decoded.userId,
+        },
+        });
+
+        if (!user) {
+        return res.status(401).json({ error: 'User not found.' });
+        }
+
         req.user = decoded;
         next();
     } catch (error) {
--- src/routes/watchlist.routes.ts
+++ src/routes/watchlist.routes.ts
@@ -10,14 +10,13 @@ const isError = (error: unknown): error is Error => {
 };
 
 // Get all watchlist items for the logged-in user
-router.get('/:userId', authenticateToken, async (req: AuthRequest, res) => {
-    const { userId } = req.params;
-    if (req.user?.userId !== parseInt(userId)) {
-        return res.status(403).json({ error: 'Access denied. Invalid user ID.' });
-    }
-
+router.get('/', authenticateToken, async (req: AuthRequest, res) => {
     try {
-        const watchlist = await getWatchlist(parseInt(userId));
+        const userId = req.user?.userId;
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const watchlist = await getWatchlist(userId);
         res.json(watchlist);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
@@ -25,44 +24,46 @@ router.get('/:userId', authenticateToken, async (req: AuthRequest, res) => {
 });
 
 // Add a new watchlist item for the logged-in user
-router.post('/:userId', authenticateToken, async (req: AuthRequest, res) => {
-    const { userId } = req.params;
-    if (req.user?.userId !== parseInt(userId)) {
-        return res.status(403).json({ error: 'Access denied. Invalid user ID.' });
-    }
-
-    const { movie_id, genres, released_year, runtime, vote_average, casts, liked } = req.body;
+router.post('/', authenticateToken, async (req: AuthRequest, res) => {
     try {
+        const userId = req.user?.userId;
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const { movie_id, genres, released_year, runtime, vote_average, casts, liked } = req.body;
         const newWatchlistItem = await addWatchlistItem({
-        user_id: parseInt(userId),
-        movie_id,
-        genres,
-        released_year,
-        runtime,
-        vote_average,
-        casts,
-        liked,
+            user_id: userId,
+            movie_id,
+            genres,
+            released_year,
+            runtime,
+            vote_average,
+            casts,
+            liked,
         });
         res.status(201).json(newWatchlistItem);
     } catch (error) {
         if (isError(error) && error.message === 'Movie is already in the watchlist') {
-        res.status(400).json({ error: error.message });
+            res.status(400).json({ error: error.message });
         } else {
-        res.status(500).json({ error: 'Internal Server Error' });
+            res.status(500).json({ error: 'Internal Server Error' });
         }
     }
 });
 
 // Delete a watchlist item by ID
-router.delete('/:userId/:id', authenticateToken, async (req: AuthRequest, res) => {
-    const { userId, id } = req.params;
-    if (req.user?.userId !== parseInt(userId)) {
-        return res.status(403).json({ error: 'Access denied. Invalid user ID.' });
-    }
+router.delete('/:id', authenticateToken, async (req: AuthRequest, res) => {
+    const { id } = req.params;
     try {
-        const result = await deleteWatchlistItem(parseInt(id), parseInt(userId));
+        const userId = req.user?.userId;
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const result = await deleteWatchlistItem(parseInt(id), userId);
         res.status(200).json(result);
-        } catch (error) {
+    } catch (error) {
         if (isError(error) && error.message === 'Watchlist item not found') {
             res.status(404).json({ error: error.message });
         } else {
