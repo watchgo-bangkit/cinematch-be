d487c7f4b22c88139c3ef9c6a7ad41d59a354d0d
Catherine Angel <catheriner40540@gmail.com>
2024-06-10 10:43:20 +0700

    refactor: update the user registration logic and update request validation schema

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import HttpError from '../utils/httpError'
+import { Prisma } from '@prisma/client'
 
 const errorHandler = (
   err: any,
@@ -10,8 +11,22 @@ const errorHandler = (
   if (err instanceof HttpError) {
     console.error(err)
     res.status(err.status).send({ error: err.message })
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    const targetedFields = err.meta?.target as string[]
+    const joinedFields = Array.isArray(targetedFields)
+      ? targetedFields.join(', ')
+      : null
+
+    if (err.code === 'P2002') {
+      res.status(400).send({
+        error: `'There is a unique constraint violation ${joinedFields ? 'on field(s) ' + joinedFields : ''}`,
+      })
+    } else {
+      res.status(400).send({
+        error: 'Prisma error occurred',
+      })
+    }
   } else {
-    console.error(err)
     res.status(500).send({ error: 'Internal Server Error' })
   }
 }
