e9731a0b28a0352ead3b1fdc30f765544417db8c
IvanLeovandi <ivanleovandi@gmail.com>
2024-06-20 20:32:49 +0700

    fix: add validation on invalid movie id when adding to watchlist

--- src/services/watchlist.service.ts
+++ src/services/watchlist.service.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import fetch from 'node-fetch';
+import { getMovieById } from '../dao/movies.dao';
 
 const prisma = new PrismaClient();
+const TMDB_API_KEY = process.env.TMDB_API_KEY || 'YOUR_TMDB_API_KEY'; // gantikan dengan API key Anda atau pastikan variabel lingkungan disetel
+
+// Fungsi untuk memvalidasi movie ID menggunakan TMDB API
+const isValidMovieId = async (movieId: number) => {
+  const data = getMovieById(movieId)
+  return data && (await data).id === movieId;
+};
 
 export const getWatchlist = async (userId: number) => {
   return await prisma.watchlist.findMany({
@@ -9,10 +18,10 @@ export const getWatchlist = async (userId: number) => {
 };
 
 export const addWatchlistItem = async (data: {
-  user_id: number,
-  movie_id: number,
-  liked: boolean,
-  is_watched: boolean,
+  user_id: number;
+  movie_id: number;
+  liked: boolean;
+  is_watched: boolean;
 }) => {
   const existingItem = await prisma.watchlist.findFirst({
     where: {
@@ -23,6 +32,11 @@ export const addWatchlistItem = async (data: {
 
   if (existingItem) {
     throw new Error('Movie is already in the watchlist');
+  }
+
+  const validMovie = await isValidMovieId(data.movie_id);
+  if (!validMovie) {
+    throw new Error('Invalid movie ID');
   }
 
   return await prisma.watchlist.create({
